1ad0b7f90c01cc2fedbb38af0d09bcd9bed24bfe
Rafael <rafaelyokoyama925@gmail.com>
2021-07-07 14:47:00 -0300

    create component Input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Input from "./components/Input";
 
 
 function App() {
   return (
     <div className="App">
-     <p>  teste</p>
+    <Input/>
     </div>
   );
 }
---
+++ src/components/Input/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+
+  return (
+    <>
+     <input name="user"  id="user" className="userInput" placeholder="User"/>
+     <button> Serach</button>
+    </>
+  );
+};
+export default Input ; 
