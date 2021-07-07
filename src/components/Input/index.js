de7c77ded2bcee0b938397bc967aa7e1787370be
Rafael <rafaelyokoyama925@gmail.com>
2021-07-07 15:23:51 -0300

    Consume API data

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,30 @@
-import React from "react";
 
+
+import React, { useState } from 'react';
+import api from '../../services/api'
 const Input = () => {
+
+    const [users, setUsers] = useState([])
+   
+ 
+ /*   function getAPI() {
+       axios.get('https://api.github.com/users/ramosht/repos').then(response => console.log(response.data))
+   }
+    
+   */
+
+   const getAPI = () => {
+    if (users !== undefined) {
+      api.get(`/${users}`)
+        .then(response => console.log(response.data))
+    } 
+  }
+ 
 
   return (
     <>
-     <input name="user"  id="user" className="userInput" placeholder="User"/>
-     <button> Serach</button>
+     <input name="user"  id="user" className="userInput" placeholder="User" onChange={(e) => setUsers(e.target.value)}/>
+     <button onClick={getAPI}>Search</button>
     </>
   );
 };
